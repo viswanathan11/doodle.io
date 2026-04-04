0707d0e6a607052711ae40e0d19520a6340e51df
Viswanathan <m.vishwanthan06@gmail.com>
2026-04-04 10:40:36 +0000

    feat: implement PlayerCard component and integrate dynamic player list rendering into the UI

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,10 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import Chat from './components/Features/Chat.jsx'
 import Players from './components/Features/Players.jsx'
-import playerCard from './components/Features/PlayerCard.jsx'
+import PlayerCard from './components/Features/PlayerCard.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <playerCard/>
-    <Players/>
+    <App/>
   </StrictMode>,
 )
