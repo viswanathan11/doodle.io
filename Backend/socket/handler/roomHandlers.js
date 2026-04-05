cdbd9fc42c89e2b4888d5d6d6001cce71b505336
Viswanathan <m.vishwanthan06@gmail.com>
2026-04-05 05:45:03 +0000

    feat: allow duplicate users to join socket room and receive current state

--- Backend/socket/handler/roomHandlers.js
+++ Backend/socket/handler/roomHandlers.js
@@ -12,7 +12,14 @@ export default function registerRoomHandlers(io, socket) {
 
         const exisitinPlayer= room.players.find(p=>p.username===username);
         if(exisitinPlayer){
-            return console.log(`[socket] rejected duplicate user: ${username}`);
+            console.log(`[socket] rejected duplicate user: ${username}`);
+               
+       // 1. Still join the socket room so they get live updates/chat
+         socket.join(code);
+    
+       // 2. Send them the room state so they can see the players!
+       return socket.emit("room:state",room);
+
         }
         const newPlayer = {
             id: socket.id,
